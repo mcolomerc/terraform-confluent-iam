3a098e1c0f9165721c0259747b20f86614cb811a
Marcelo Colomer <mcolomercornejo@confluent.io>
2023-08-28 18:44:57 +0200

    optional

--- variables.tf
+++ variables.tf
@@ -27,8 +27,8 @@ variable environment {
 variable sa_role_bindings {
     type = list(object({
       role = string
-      optional(resource) = string   
-      optional(name) = string
+      resource = optional(string)   
+      name = optional(string)
     }))
     default = []
     description = "Service Account Role Bindings. List of: Role name (DeveloperRead, DeveloperWrite, ResourceOwner), resource: (topic, group or transactional-id), name: (name of the resource)"
