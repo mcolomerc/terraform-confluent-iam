cd0809a27d86d272c8366c2ff958316583078c11
Marcelo Colomer <mcolomercornejo@confluent.io>
2023-08-29 16:44:12 +0200

    CloudClusterAdmin

--- main.tf
+++ main.tf
@@ -53,17 +53,17 @@ resource "confluent_api_key" "service-account-kafka-api-key" {
 resource "confluent_role_binding" "cluster_admin" { 
   for_each   = {
     for index, rbac in var.sa_role_bindings:
-    rbac.name => rbac if rbac.role == "ClusterAdmin" 
+    rbac.name => rbac if rbac.role == "CloudClusterAdmin" 
   }
   principal   = "User:${confluent_service_account.sa.id}"
-  role_name   = "ClusterAdmin"
+  role_name   = "CloudClusterAdmin"
   crn_pattern = "${data.confluent_kafka_cluster.cluster.rbac_crn}"
 }
 
 resource "confluent_role_binding" "cluster_resource_rbac" { 
   for_each   = {
     for index, rbac in var.sa_role_bindings:
-    rbac.name => rbac  if rbac.role != "ClusterAdmin"
+    rbac.name => rbac  if rbac.role != "CloudClusterAdmin"
   }
   principal   = "User:${confluent_service_account.sa.id}"
   role_name   = each.value.role
